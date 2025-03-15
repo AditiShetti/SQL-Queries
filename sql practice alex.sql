2645c95077044b5624ee7fe54b9913b2d2e15e7b
Aditi Shetti <144339282+AditiShetti@users.noreply.github.com>
2025-03-15 23:51:01 +0530

    Update sql practice alex.sql

--- sql practice alex.sql
+++ sql practice alex.sql
@@ -7,7 +7,7 @@ Alex the Analyst
 tesla_model	 car_price	 cars_sold 	production_cost
 Model 3     	 46990	        156357	  	  	21000
 Model S	       104990        	23464	  	    	43000
-Model X	       120990	  	  19542	  	    	48000...
+Model X	       120990	  	19542	  	    	48000...
 
 
 tesla_model	 car_price	 cars_sold 	production_cost

8d0c78a64430a010cc5b9c97e48c767928a8e744
Aditi Shetti <144339282+AditiShetti@users.noreply.github.com>
2025-03-16 01:13:26 +0530

    Update sql practice alex.sql

--- sql practice alex.sql
+++ sql practice alex.sql
@@ -22,8 +22,8 @@ order by profit desc limit 1;
   then they are at high risk of having a heart attack.  Order by Cholesterol high to low.
  
  patient_id	  age	  cholesterol 	weight
-1001	        52     	373        	267
-1002	        93    	 235	        164
+1001	        52       	373        	267
+1002	        93    	   235	        164
 1003	        77	    324	        211...
 
 SELECT *

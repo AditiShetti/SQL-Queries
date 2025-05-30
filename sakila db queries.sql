10c4ae9bc3a6228bea7ff144b87ab140a9ade8dd
Aditi Shetti <144339282+AditiShetti@users.noreply.github.com>
2025-05-30 16:42:25 +0530

    Add files via upload

--- sakila db queries.sql
+++ sakila db queries.sql
@@ -62,18 +62,21 @@ select * from rental order by rental_id desc;
 
 select * from customer;
 select * from staff ;
-select * from rental;
+select * from rental order by rental_id desc;
 select * from film;
-
-
-1111111
--- 9. When is ‘Academy Dinosaur’ due?
+ 
+-- Deleting the previous row.
+delete from rental where rental_id= 16050;
+ 
+ 
+-- 9. When is ‘Academy Dinosaur’ due? # Due date would be the rental date + rental_duration (in days)
 #Finding the film id first
-select *
-from film  f
+select f.film_id,f.title,r.rental_id, r.rental_date, r.return_date, f.rental_duration,
+       date_add(r.rental_date, interval f.rental_duration day) as due_date  # for date only add date() before date_add
+from film  f  
 join inventory i  on f.film_id= i.film_id
 join rental r     on i.inventory_id= r.inventory_id
-where title = 'Academy Dinosaur' order by r.rental_date desc;
+where title = 'Academy Dinosaur' order by r.rental_date desc limit 1;
  
 
 -- 10. What is the average running time of all the films in the sakila DB?
@@ -90,6 +93,7 @@ order by avg(f.length) desc;
 
 
 -- 12. Why does this query return the empty set?  select * from film natural join inventory;
+# A natural join combines tables based on common columns that share the same name and have compatible dtypes.
 
 -- 13. Which actors participated in the movie ‘Academy Dinosaur’? Print their first and last names.
 select a.first_name , a.last_name, a.actor_id
@@ -121,70 +125,75 @@ from payment p join staff st on p.staff_id= st.staff_id
      join store s on st.store_id= s.store_id
 group by s.store_id   
  
-111111 
--- 20. Find pairs of actors that participated together in the same movie and print their full names. Each such pair should appear only once in the result. (You should have 10,385 rows in the result)
-select fa1.actor_id as actor_id1,fa1.actor_id as actor_id1 , fa1.film_id 
-from film_actor fa1 
-join film_actor fa2 on fa1.film_id > fa2.film_id
+ 
+-- 18. Find pairs of actors that participated together in the same movie and print their full names. Each such pair should appear only once in the result. (You should have 10,385 rows in the result)
+select fa1.actor_id as actor_id1,
+	concat(a1.first_name," ",a1.last_name) as actor1_name,
+       fa2.actor_id as actor_id2 , 
+	concat(a2.first_name," ",a2.last_name) as actor2_name,
+       fa1.film_id , f.title as movie_title
+from film_actor fa1  
+join film_actor fa2 on fa1.film_id = fa2.film_id and fa1.actor_id< fa2.actor_id
+join actor a1 on fa1.actor_id = a1.actor_id
+join actor a2 on fa2.actor_id = a2.actor_id
+join film f on f.film_id = fa1.film_id;
 
--- 21. Display the top five most popular films, i.e, films that were rented the highest number of times. For each film print its title and the number of times it was rented.
+-- 19. Display the top five most popular films, i.e, films that were rented the highest number of times. For each film print its title and the number of times it was rented.
 select i.film_id, f.title, count(*) as rental_count 
 from rental r 
 join inventory i on r.inventory_id= i.inventory_id 
 join film f on f.film_id = i.film_id 
 group by i.film_id, f.title order by rental_count desc limit 5;
 
--- 22. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
+-- 20. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
 select Upper(concat(first_name ," ", last_name)) as Actor_Name from actor 
 
--- 23a. Find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe."
+-- 21a. Find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe."
 select actor_id,first_name, last_name  from actor where first_name='Joe';
 
--- 23b. Find all actors whose last name contain the letters GEN.
+-- 21b. Find all actors whose last name contain the letters GEN.
 select actor_id,first_name, last_name  from actor where last_name like '%GEN%';
 
--- 23c. Find all actors whose last names contain the letters LI. Order the rows by last name and first name.
+-- 21c. Find all actors whose last names contain the letters LI. Order the rows by last name and first name.
 select actor_id,first_name, last_name from actor where last_name like '%LI%' order by last_name , first_name;
 
--- 23d. Display the country_id and country of Afghanistan, Bangladesh, and China using IN.
+-- 21d. Display the country_id and country of Afghanistan, Bangladesh, and China using IN.
 select country_id, country from country where country in ('Afghanistan', 'Bangladesh', 'China');
 
 
-#24.
--- 24a. Add a column in the table actor named description with data type BLOB.
+#22.
+-- 22a. Add a column in the table actor named description with data type BLOB.
 alter table actor add column description blob;
 
 select * from actor ;
--- 24b. Delete the description column from the actor table.
+-- 22b. Delete the description column from the actor table.
 alter table actor drop column description;
 
-#25.
--- 25a. List the last names of actors, as well as how many actors have that last name.
+#23.
+-- 23a. List the last names of actors, as well as how many actors have that last name.
 select last_name, count(last_name) as count_lastname 
 from actor group by last_name order by count_lastname desc; 
--- 25b. List last names of actors and the number of actors who have that last name, but only for names shared by at least two actors.
+-- 23b. List last names of actors and the number of actors who have that last name, but only for names shared by at least two actors.
 select last_name, count(last_name) as count_lastname from actor
  group by last_name having  count(last_name)>=2 order by count_lastname desc; 
--- 25c. Update actor HARPO WILLIAMS who was mistakenly entered as GROUCHO WILLIAMS.
+-- 23c. Update actor HARPO WILLIAMS who was mistakenly entered as GROUCHO WILLIAMS.
 Update actor set first_name = 'HARPO' where first_name = 'GROUCHO' and last_name = 'WILLIAMS'; #If last_nam condition is not written it will update all 'HARPO'
--- 25d. Revert the name from HARPO back to GROUCHO if the actor is currently listed as HARPO.
+-- 23d. Revert the name from HARPO back to GROUCHO if the actor is currently listed as HARPO.
 update actor set first_name ='GROUCHO' where first_name= 'HARPO' and last_name = 'WILLIAMS';  
 
 
--- 26 Find the schema (CREATE statement) of the address table.
+-- 24. Find the schema (CREATE statement) of the address table.
 describe address;
 show create table address;
 
 
-#27.
--- 27a. Use JOIN to display the first and last names, as well as the address, of each staff member.
+#25.
+-- 25a. Use JOIN to display the first and last names, as well as the address, of each staff member.
 select s.first_name,s.last_name,a.* 
 from staff s 
 join address a on a.address_id= s.address_id;
 
-
-
--- 27b. Use JOIN to display the total amount rung up by each staff member in August of 2005.
+-- 25b. Use JOIN to display the total amount rung up by each staff member in August of 2005.
 select s.staff_id, sum(p.amount) as total_amount
 from staff s
 join payment p on s.staff_id= p.staff_id 
@@ -192,37 +201,37 @@ where year(p.payment_date)= 2005 and month(p.payment_date)=8
 group by s.staff_id ; 
 
 select * from payment;
--- 27c. List each film and the number of actors who are listed for that film.
+-- 25c. List each film and the number of actors who are listed for that film.
 select f.title, count(fa.actor_id) as actor_count from film f 
 join film_actor fa on f.film_id= fa.film_id group by f.film_id,f.title;
 
--- 27d. How many copies of the film *Hunchback Impossible* exist in the inventory system?
+-- 25d. How many copies of the film *Hunchback Impossible* exist in the inventory system?
 select count(*)
 from film f 
 join inventory i on f.film_id= i.film_id 
 where f.title='Hunchback Impossible';
 
--- 27e. List the total paid by each customer, ordered by last name.
+-- 25e. List the total paid by each customer, ordered by last name.
 select c.first_name, c.last_name, sum(p.amount) as amount
 from customer c 
 join payment p on c.customer_id= p.customer_id group by c.first_name, c.last_name 
 order by c.last_name desc;
 
--- 28a. Display titles of movies starting with the letters K and Q whose language is English using subqueries.
+-- 26a. Display titles of movies starting with the letters K and Q whose language is English using subqueries.
 select title 
 from film 
 where (title like 'K%' or title like 'Q%' )
 and language_id in 
           (select language_id from language where name='English')
 
--- 28b. Display all actors who appear in the film *Alone Trip* using subqueries.
+-- 26b. Display all actors who appear in the film *Alone Trip* using subqueries.
 select fa.actor_id, a.first_name, a.last_name
 from film_actor fa 
 join actor a on a.actor_id= fa.actor_id
 where fa.film_id in 
 (select film_id from film where title ='Alone Trip')
 
--- 28c. Retrieve the names and email addresses of all Canadian customers for a marketing campaign.
+-- 26c. Retrieve the names and email addresses of all Canadian customers for a marketing campaign.
 select cu.first_name, cu.last_name, cu.email
 from customer cu  
 join address a     on cu.address_id= a.address_id
@@ -230,28 +239,28 @@ join city c        on a.city_id= c.city_id
 join country co    on c.country_id= co.country_id 
 where co.country='Canada';
 
--- 28d. Identify all movies categorized as Family films.
+-- 26d. Identify all movies categorized as Family films.
 select f.film_id , f.title
 from film f 
 join film_category fc on f.film_id = fc.film_id
 join category c on fc.category_id = c.category_id where c.name='Family'
 
 
--- 28e. Display how much business, in dollars, each store brought in.
+-- 26e. Display how much business, in dollars, each store brought in.
  select s.store_id, sum(p.amount) as revenue
  from store s 
   join staff st on s.store_id =st.store_id
   join payment p on  st.staff_id=p.staff_id
   group by  s.store_id
   
-  -- 28f. Display for each store: its store ID, city, and country.
+  -- 26f. Display for each store: its store ID, city, and country.
  select s.store_id, c.city,co.country 
  from store s 
   join address a on s.address_id = a.address_id
   join city c on a.city_id= c.city_id
   join country co on c.country_id = co.country_id;
   
--- 28g. List the top five genres in gross revenue in descending order.
+-- 26g. List the top five genres in gross revenue in descending order.
 select c.name, sum(p.amount) as revenue
 from category c
 join film_category fc on c.category_id= fc.category_id
@@ -273,9 +282,12 @@ select * from sales_by_film_category;
 select * from sales_by_store;
 select * from staff_list;
 
+
+describe actor_info
+
 -- Create all the above view again acc to understanding.
 
--- 29a. Create a view to show the top five genres by gross revenue.
+-- 27a. Create a view to show the top five genres by gross revenue.
 create or replace view top_5_genres_revenue as
 (select c.name, sum(p.amount) as revenue
 from category c
@@ -289,15 +301,31 @@ group by c.name,c.category_id order by sum(p.amount) desc limit 5)
 select * from top_5_genres_revenue
 show full tables where table_type= 'VIEW';
 
--- 29b. Display the view created in 29a.
+-- 27b. Display the view created in 29a.
 select * from top_5_genres_revenue
 
--- 29c. Drop the view created in 29a if no longer needed.
+-- 27c. Drop the view created in 29a if no longer needed.
 drop view top_5_genres_revenue;
 
+-- 28. Create a view showing each film’s rental count
+create or replace view film_rental as 
+(select f.film_id, f.title , count(r.rental_id) as rented_count
+from film f 
+left join inventory i on f.film_id= i.film_id
+left join rental r on  i.inventory_id= r.inventory_id
+group by f.film_id, f.title
+order by count(r.rental_id) desc) 
 
+select * from film_rental where rented_count>30;
 
 # Self join qns
-# Views. Create the views again . Perform view operations.
+-- 29.Write a query to find pairs of actors (actor_id1, actor_id2) who have the same last name but different actor_ids.
+select a1.first_name,a1.actor_id, a1.last_name, a2.first_name,a2.actor_id 
+from actor a1
+join actor a2
+on a1.last_name = a2.last_name
+and a1.actor_id > a2.actor_id 
+
+
 # timestamp funcs
 # label address as store / staff add.

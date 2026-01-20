fcadde153324606736658a0a62dae1d684ffbbac
Aditi Shetti <144339282+AditiShetti@users.noreply.github.com>
2026-01-20 16:58:12 +0530

    Update sql practice alex.sql

--- sql practice alex.sql
+++ sql practice alex.sql
@@ -9,11 +9,7 @@ Model 3     	 46990	        156357	  	  	21000
 Model S	       104990        	23464	  	    	43000
 Model X	       120990	  	19542	  	    	48000...
 
-
-tesla_model	 car_price	 cars_sold 	production_cost
-Model 3     	46990	      156357	  	  	21000      
-Model S	       104990       	23464	  	       	43000      
-Model X	       120990	  	19542	  	    	48000...  
+ 
 SELECT *, (car_price-production_cost)*cars_sold as profit
 FROM tesla_models
 order by profit desc limit 1;
